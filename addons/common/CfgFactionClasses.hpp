384d4155cb9fc58cf1e2437096a56b276b9292ab
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-03-06 17:48:30 +0100

    Added and fixed icon and texture

--- addons/common/CfgFactionClasses.hpp
+++ addons/common/CfgFactionClasses.hpp
@@ -3,16 +3,16 @@ class CfgFactionClasses {
     class BLU_USA_7CAV_F {
         displayName = CSTRING(Faction_Display);
         author = ECSTRING(main,CavModTeam);
-        flag = QPATHTOF(data\flag_7cav_co.paa);
-        icon = QPATHTOF(data\cfgfactionclasses_7cav_ca.paa);
+        flag = QPATHTOF(data\faction_cav_flag_co.paa);
+        icon = QPATHTOF(data\faction_cav_icon_co.paa);
         priority = 0;
         side = 0;
     };
     class IND_USA_7CAV_F {
         displayName = CSTRING(Faction_Display);
         author = ECSTRING(main,CavModTeam);
-        flag = QPATHTOF(data\flag_7cav_co.paa);
-        icon = QPATHTOF(data\cfgfactionclasses_7cav_ca.paa);
+        flag = QPATHTOF(data\faction_cav_flag_co.paa);
+        icon = QPATHTOF(data\faction_cav_icon_co.paa);
         priority = 0;
         side = 2;
     };
