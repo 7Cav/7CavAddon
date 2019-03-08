c7ecc849954cc56fb3158b6d883a1a7955241726
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-03-08 21:45:00 +0100

    fixed settings

--- addons/main/CfgSettings.hpp
+++ addons/main/CfgSettings.hpp
@@ -1,7 +1,7 @@
 class CfgSettings {
     class CBA {
         class Versioning {
-            class BlackOrder {
+            class CavMod {
                 class dependencies {
                     //Mod will hard exit if this is missing
                     CBA[] = {"cba_main", REQUIRED_CBA_VERSION, "(true)"};
