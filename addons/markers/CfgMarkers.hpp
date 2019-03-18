63391bbf7fc4b3afa8955a3b7303a80949937a77
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-03-18 18:52:33 +0100

    Fixed classname

--- addons/markers/CfgMarkers.hpp
+++ addons/markers/CfgMarkers.hpp
@@ -1,11 +1,11 @@
 class CfgMarkers {
     class flag_NATO;
-    class cavmod_flag_cav : flag_NATO {
+    class cavmod_flag_cav_00 : flag_NATO {
         name = CSTRING(flag_cav_00);
         icon = QPATHTOF(data\marker_cav_00_ca.paa);
         texture = QPATHTOF(data\marker_cav_00_ca.paa);
     };
-    class cavmod_flag_cav : flag_NATO {
+    class cavmod_flag_cav_01 : flag_NATO {
         name = CSTRING(flag_cav_01);
         icon = QPATHTOF(data\marker_cav_01_ca.paa);
         texture = QPATHTOF(data\marker_cav_01_ca.paa);
