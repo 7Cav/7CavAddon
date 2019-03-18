790370cacebb81b0bd47fc9e623d14f68cf5f5f4
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-03-18 18:50:02 +0100

    Added markers

---
+++ addons/markers/CfgMarkers.hpp
@@ -0,0 +1,13 @@
+class CfgMarkers {
+    class flag_NATO;
+    class cavmod_flag_cav : flag_NATO {
+        name = CSTRING(flag_cav_00);
+        icon = QPATHTOF(data\marker_cav_00_ca.paa);
+        texture = QPATHTOF(data\marker_cav_00_ca.paa);
+    };
+    class cavmod_flag_cav : flag_NATO {
+        name = CSTRING(flag_cav_01);
+        icon = QPATHTOF(data\marker_cav_01_ca.paa);
+        texture = QPATHTOF(data\marker_cav_01_ca.paa);
+    };
+};
---
+++ addons/markers/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT markers
+#define COMPONENT_BEAUTIFIED Markers
+
+#include "\z\cavmod\addons\main\script_mod.hpp"
+#include "\z\cavmod\addons\main\script_macros.hpp"
