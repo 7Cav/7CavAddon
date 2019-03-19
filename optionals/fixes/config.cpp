a3bd2f432f338ebbd18f5fe1b96af456a8ddc4bc
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-03-19 15:08:53 +0100

    Added fixes addon for optional usage

---
+++ optionals/fixes/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "cavmod_main",
+            "cavmod_common"
+        };
+        author = ECSTRING(main,CavModTeam);
+        authors[] = {"Brostrom.A (Evul)"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
---
+++ optionals/fixes/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT fixes
+#define COMPONENT_BEAUTIFIED Bugfixes and Adjustments
+
+#include "\z\cavmod\addons\main\script_mod.hpp"
+#include "\z\cavmod\addons\main\script_macros.hpp"
