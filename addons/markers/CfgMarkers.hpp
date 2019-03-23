d8fb587a27cb848d3e38faf073585dc7132db776
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-03-23 15:07:40 +0100

    added cav markers

--- addons/markers/CfgMarkers.hpp
+++ addons/markers/CfgMarkers.hpp
@@ -10,4 +10,24 @@ class CfgMarkers {
         icon = QPATHTOF(data\marker_cav_01_ca.paa);
         texture = QPATHTOF(data\marker_cav_01_ca.paa);
     };
+
+    class hd_dot;
+	class cavmod_hd_cav : hd_dot {
+        name = CSTRING(handdrawn_cav);
+        icon = QPATHTOF(data\handdrawn_cav_ca.paa);
+	};
+	class cavmod_hd_cav_noShadow : cavmod_hd_cav {
+		scope = 0;
+		shadow = 0;
+	};
+
+    class mil_objective;
+	class cavmod_mil_cav : mil_objective {
+        name = CSTRING(military_cav);
+        icon = QPATHTOF(data\military_cav_ca.paa);
+	};
+	class cavmod_mil_cav_noShadow: cavmod_mil_cav {
+		scope = 0;
+		shadow = 0;
+	};
 };
