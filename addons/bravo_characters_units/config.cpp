81663bc3d32a29ef9dca4a47ecb70c7f81f59170
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-03-27 21:09:28 +0100

    Fixed and adjustments

--- addons/bravo_characters_units/config.cpp
+++ addons/bravo_characters_units/config.cpp
@@ -42,6 +42,7 @@ class CfgPatches {
         requiredAddons[] = {
             "cav_main",
             "cav_common",
+            
             "cav_bravo_characters"
         };
         author = ECSTRING(main,CavModTeam);
