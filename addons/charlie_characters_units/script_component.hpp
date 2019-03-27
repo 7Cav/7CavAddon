f14b7b4d9a571f6c72c1b8e595587f0577a2f511
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-03-27 22:31:34 +0100

    Fixed script compinent

--- addons/charlie_characters_units/script_component.hpp
+++ addons/charlie_characters_units/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT charlie_characters_units
-#define COMPONENT_BEAUTIFIED Charlie Unit
+#define COMPONENT_BEAUTIFIED Charlie Units
 
 #include "\z\cav\addons\main\script_mod.hpp"
 #include "\z\cav\addons\main\script_macros.hpp"
