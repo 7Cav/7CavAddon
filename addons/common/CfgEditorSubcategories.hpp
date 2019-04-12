b28f234fb3ab5185acf978953a6c77bee5dca21c
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-04-12 20:07:21 +0200

    Added category to be used in the composition menu

---
+++ addons/common/CfgEditorSubcategories.hpp
@@ -0,0 +1,11 @@
+class CfgEditorSubcategories {
+    class Cav_EdSubcat_Deploy_Platoon {
+        displayname = CSTRING(Cat_Deploy_Platoon); 
+    };
+    class Cav_EdSubcat_Personnel_Weap_Bravo {
+        displayname = CSTRING(Cat_Bravo_Company_Inf_Weap); 
+    };
+    class Cav_EdSubcat_Personnel_Logi_Bravo {
+        displayname = CSTRING(Cat_Bravo_Company_Logi); 
+    };
+};
