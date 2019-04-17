42a89a14446dcb1290dc436621356483ef3e0dcc
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-04-17 13:17:03 +0200

    Added addon for alpha vehicles

---
+++ addons/alpha_vehicles/config.cpp
@@ -0,0 +1,22 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {
+        };
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "cav_main",
+            "cav_common"
+        };
+        author = ECSTRING(main,CavModTeam);
+        authors[] = {"Brostrom.A (Evul)"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEditorSubcategories.hpp"
+#include "CfgVehicles.hpp"
