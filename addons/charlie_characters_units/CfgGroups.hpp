326d00ba8287978af6279a4059ac04b738360eaa
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-05-16 15:07:10 +0200

    Removed havoc

--- addons/charlie_characters_units/CfgGroups.hpp
+++ addons/charlie_characters_units/CfgGroups.hpp
@@ -561,66 +561,66 @@ class CfgGroups {
                 };
 
 
-                class Cav_B_C_Havoc_1_Squad {
-                    name = CSTRING(C_Squad_Havoc_1);
-                    icon = "\A3\ui_f\data\map\markers\nato\b_support.paa";
-                    faction = "BLU_USA_7CAV_F";
-                    side = 1;
-                    class Unit0 {
-                        position[] = {0, 0, 0};
-                        rank = "SERGEANT";
-                        side = 1;
-                        vehicle = "Cav_B_C_Weap_SquadLeader_Havoc_1_F";
-                    };
-                    class Unit1 {
-                        position[] = {1.5, -1.0, 0.0};
-                        rank = "CORPORAL";
-                        side = 1;
-                        vehicle = "Cav_B_C_Weap_Alpha_FireTeamLeader_F";
-                    };
-                    class Unit2 {
-                        position[] = {0.5, -1.0, 0.0};
-                        rank = "PRIVATE";
-                        side = 1;
-                        vehicle = "Cav_B_C_Weap_Alpha_AutomaticRifleman_F";
-                    };
-                    class Unit3 {
-                        position[] = {-0.5, -1.0, 0.0};
-                        rank = "PRIVATE";
-                        side = 1;
-                        vehicle = "Cav_B_C_Weap_Alpha_Grenadier_F";
-                    };
-                    class Unit4 {
-                        position[] = {-1.5, -1.0, 0.0};
-                        rank = "PRIVATE";
-                        side = 1;
-                        vehicle = "Cav_B_C_Weap_Alpha_Rifleman_F";
-                    };
-                    class Unit5 {
-                        position[] = {1.5, -2.0, 0.0};
-                        rank = "CORPORAL";
-                        side = 1;
-                        vehicle = "Cav_B_C_Weap_Bravo_FireTeamLeader_F";
-                    };
-                    class Unit6 {
-                        position[] = {0.5, -2.0, 0.0};
-                        rank = "PRIVATE";
-                        side = 1;
-                        vehicle = "Cav_B_C_Weap_Bravo_AutomaticRifleman_F";
-                    };
-                    class Unit7 {
-                        position[] = {-0.5, -2.0, 0.0};
-                        rank = "PRIVATE";
-                        side = 1;
-                        vehicle = "Cav_B_C_Weap_Bravo_Grenadier_F";
-                    };
-                    class Unit8 {
-                        position[] = {-1.5, -2.0, 0.0};
-                        rank = "PRIVATE";
-                        side = 1;
-                        vehicle = "Cav_B_C_Weap_Bravo_CombatLifeSaver_F";
-                    };
-                };
+                // class Cav_B_C_Havoc_1_Squad {
+                //     name = CSTRING(C_Squad_Havoc_1);
+                //     icon = "\A3\ui_f\data\map\markers\nato\b_support.paa";
+                //     faction = "BLU_USA_7CAV_F";
+                //     side = 1;
+                //     class Unit0 {
+                //         position[] = {0, 0, 0};
+                //         rank = "SERGEANT";
+                //         side = 1;
+                //         vehicle = "Cav_B_C_Weap_SquadLeader_Havoc_1_F";
+                //     };
+                //     class Unit1 {
+                //         position[] = {1.5, -1.0, 0.0};
+                //         rank = "CORPORAL";
+                //         side = 1;
+                //         vehicle = "Cav_B_C_Weap_Alpha_FireTeamLeader_F";
+                //     };
+                //     class Unit2 {
+                //         position[] = {0.5, -1.0, 0.0};
+                //         rank = "PRIVATE";
+                //         side = 1;
+                //         vehicle = "Cav_B_C_Weap_Alpha_AutomaticRifleman_F";
+                //     };
+                //     class Unit3 {
+                //         position[] = {-0.5, -1.0, 0.0};
+                //         rank = "PRIVATE";
+                //         side = 1;
+                //         vehicle = "Cav_B_C_Weap_Alpha_Grenadier_F";
+                //     };
+                //     class Unit4 {
+                //         position[] = {-1.5, -1.0, 0.0};
+                //         rank = "PRIVATE";
+                //         side = 1;
+                //         vehicle = "Cav_B_C_Weap_Alpha_Rifleman_F";
+                //     };
+                //     class Unit5 {
+                //         position[] = {1.5, -2.0, 0.0};
+                //         rank = "CORPORAL";
+                //         side = 1;
+                //         vehicle = "Cav_B_C_Weap_Bravo_FireTeamLeader_F";
+                //     };
+                //     class Unit6 {
+                //         position[] = {0.5, -2.0, 0.0};
+                //         rank = "PRIVATE";
+                //         side = 1;
+                //         vehicle = "Cav_B_C_Weap_Bravo_AutomaticRifleman_F";
+                //     };
+                //     class Unit7 {
+                //         position[] = {-0.5, -2.0, 0.0};
+                //         rank = "PRIVATE";
+                //         side = 1;
+                //         vehicle = "Cav_B_C_Weap_Bravo_Grenadier_F";
+                //     };
+                //     class Unit8 {
+                //         position[] = {-1.5, -2.0, 0.0};
+                //         rank = "PRIVATE";
+                //         side = 1;
+                //         vehicle = "Cav_B_C_Weap_Bravo_CombatLifeSaver_F";
+                //     };
+                // };
 
             };
         };
