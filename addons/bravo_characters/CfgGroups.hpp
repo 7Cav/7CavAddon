a5b9f704a1999515f0ada0406010aa426b97b28e
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-09-23 19:56:12 +0200

    Fixed lancer

--- addons/bravo_characters/CfgGroups.hpp
+++ addons/bravo_characters/CfgGroups.hpp
@@ -4,7 +4,68 @@ class CfgGroups {
             name = ECSTRING(common,Faction_Display);
             class Cav_Bravo_Infantry_Squads {
                 name = CSTRING(Cat_Bravo_Company_Inf);
-                
+
+                class Cav_B_B_Lancer_Squad {
+                    name = CSTRING(B_Squad_Lancer);
+                    icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+                    faction = "BLU_USA_7CAV_F";
+                    side = 1;
+                    class Unit0 {
+                        position[] = {0, 0, 0};
+                        rank = "SERGEANT";
+                        side = 1;
+                        vehicle = "Cav_B_B_SquadLeader_F";
+                    };
+                    class Unit1 {
+                        position[] = {1.5, -1.0, 0.0};
+                        rank = "CORPORAL";
+                        side = 1;
+                        vehicle = "Cav_B_B_Alpha_FireTeamLeader_F";
+                    };
+                    class Unit2 {
+                        position[] = {0.5, -1.0, 0.0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "Cav_B_B_Alpha_AutomaticRifleman_F";
+                    };
+                    class Unit3 {
+                        position[] = {-0.5, -1.0, 0.0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "Cav_B_B_Alpha_Grenadier_F";
+                    };
+                    class Unit4 {
+                        position[] = {-1.5, -1.0, 0.0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "Cav_B_B_Alpha_Rifleman_F";
+                    };
+                    class Unit5 {
+                        position[] = {1.5, -2.0, 0.0};
+                        rank = "CORPORAL";
+                        side = 1;
+                        vehicle = "Cav_B_B_Bravo_FireTeamLeader_F";
+                    };
+                    class Unit6 {
+                        position[] = {0.5, -2.0, 0.0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "Cav_B_B_Bravo_AutomaticRifleman_F";
+                    };
+                    class Unit7 {
+                        position[] = {-0.5, -2.0, 0.0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "Cav_B_B_Bravo_Grenadier_F";
+                    };
+                    class Unit8 {
+                        position[] = {-1.5, -2.0, 0.0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "Cav_B_B_Bravo_CombatLifeSaver_F";
+                    };
+                };
+
                 class Cav_B_B_Viking_Squad {
                     name = CSTRING(B_Squad_Viking);
                     icon = "\A3\ui_f\data\map\markers\nato\b_mech_inf.paa";
