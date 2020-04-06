b745c28003df50e818757beab3d71a7e2727d6ee
ColdEvul <andreas.brostrom.ce@gmail.com>
2020-04-06 20:51:59 +0200

    Fixed training unis

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 #define MAJOR 0
 #define MINOR 0
 #define PATCHLVL 0
 #define BUILD 0
+=======
+#define MAJOR 1
+#define MINOR 2
+#define PATCHLVL 4
+#define BUILD 25
+>>>>>>> b940845... Fixed training unis
