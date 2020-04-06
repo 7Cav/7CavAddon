111d8fc424ca8d7261cbe130320ac74151ae9245
ColdEvul <andreas.brostrom.ce@gmail.com>
2020-04-06 20:51:59 +0200

    Fixed training unis

---
+++ addons/characters_training/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT characters_training
+#define COMPONENT_BEAUTIFIED Traning Characters
+
+#include "\z\cav\addons\main\script_mod.hpp"
+#include "\z\cav\addons\main\script_macros.hpp"
