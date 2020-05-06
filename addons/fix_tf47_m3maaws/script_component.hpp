5657c1276ba9f20c23ab4fd5c0a12f5b9aee4fd5
ColdEvul <andreas.brostrom.ce@gmail.com>
2020-05-06 23:17:03 +0200

    Removed dependency for TF47 and USAF

--- addons/fix_tf47_m3maaws/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT fix_tf47_m3maaws
-#define COMPONENT_BEAUTIFIED Adjustments for TF47 MAAWS
-
-#include "\z\cav\addons\main\script_mod.hpp"
-#include "\z\cav\addons\main\script_macros.hpp"
