48a3e6920a0c4f810e14382eeee3c3f02bb8d82f
ColdEvul <andreas.brostrom.ce@gmail.com>
2020-05-08 16:18:26 +0200

    Renamed moduel

--- addons/troops_training/script_component.hpp
+++ addons/troops_training/script_component.hpp
@@ -1,5 +1,5 @@
-#define COMPONENT characters_training
-#define COMPONENT_BEAUTIFIED Traning Characters
+#define COMPONENT troops_training
+#define COMPONENT_BEAUTIFIED Training Troops
 
 #include "\z\cav\addons\main\script_mod.hpp"
 #include "\z\cav\addons\main\script_macros.hpp"
