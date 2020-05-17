611663387dd24b73b9b21ad860e7ade75526ba4b
ColdEvul <andreas.brostrom.ce@gmail.com>
2020-05-17 21:52:04 +0200

    Added wip cTab fix

--- optionals/fix_cTab/cTab.hpp
+++ optionals/fix_cTab/cTab.hpp
@@ -6,3 +6,43 @@ class cTabUavMap : cTab_Tablet_RscMapControl {
 class cTab_RscMapControl {
     widthRailWay = 1;
 };
+class cTab_Android_dlg {
+    class controlsBackground {
+        class cTab_android_RscMapControl;
+        class screen: cTab_android_RscMapControl {
+            lineMarker = 1;
+            lineWidthThin = 1;
+            lineWidthThick = 1;
+            lineDistanceMin = 1;
+            lineLengthMin = 1;
+        };
+        class screenTopo: screen {
+            lineMarker = 1;
+            lineWidthThin = 1;
+            lineWidthThick = 1;
+            lineDistanceMin = 1;
+            lineLengthMin = 1;
+        };
+    };
+};
+class RscTitles {
+    class cTab_Android_dsp {
+        class controlsBackground {
+            class cTab_android_RscMapControl;
+            class screen: cTab_android_RscMapControl {
+                lineMarker = 1;
+                lineWidthThin = 1;
+                lineWidthThick = 1;
+                lineDistanceMin = 1;
+                lineLengthMin = 1;
+            };
+            class screenTopo: screen {
+                lineMarker = 1;
+                lineWidthThin = 1;
+                lineWidthThick = 1;
+                lineDistanceMin = 1;
+                lineLengthMin = 1;
+            };
+        };
+    };
+};
