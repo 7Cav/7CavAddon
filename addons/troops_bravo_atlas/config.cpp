3119d604a97e673e4c23f611c55f5712186c5173
ColdEvul <andreas.brostrom.ce@gmail.com>
2020-05-28 23:16:14 +0200

    Fixed classnames

--- addons/troops_bravo_atlas/config.cpp
+++ addons/troops_bravo_atlas/config.cpp
@@ -4,19 +4,26 @@ class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
         units[] = {
-            "Cav_B_Scout_base_F",
-            "Cav_B_B_Scout_Platoon_Medic_F",
-            "Cav_B_B_Scout_Trooper_F",
-            "Cav_B_B_Scout_Officer_F",
-            "Cav_B_B_Scout_Team_Leader_F",
-            "Cav_B_B_Scout_Crew_F",
-            "Cav_B_B_Scout_Gunner_F",
-            "Cav_B_B_Scout_Driver_F"
+    "Cav_B_Bravo_Atlas_base_F",
+    "Cav_B_B_Atlas_Medic_TeamLeader_F",
+    "Cav_B_B_Atlas_Medic_CombatMedic_F",
+    "Cav_B_B_Atlas_Logistics_Officer_F",
+    "Cav_B_B_Atlas_Logistics_OpsOfficer_F",
+    "Cav_B_B_Atlas_Logistics_OpsNCO_F",
+    "Cav_B_B_Atlas_Logistics_TeamLeader_F",
+    "Cav_B_B_Atlas_Logistics_TeamMember_F",
+    "Cav_B_B_Atlas_Medic_TeamLeader_3_1_F",
+    "Cav_B_B_Atlas_Medic_TeamLeader_3_2_F",
+    "Cav_B_B_Atlas_Logistics_TeamLeader_3_3_F",
+    "Cav_B_B_Atlas_Logistics_TeamLeader_3_4_F",
+    "Cav_B_B_Atlas_Logistics_OpsNCO_3_5_F",
+    "Cav_B_B_Atlas_Logistics_OpsOfficer_3_6_F"
         };
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
-            "cav_bravo_characters"
+            "cav_main",
+            "cav_common"
         };
         author = ECSTRING(main,cavmodteam);
         authors[] = {"Brostrom.A (Evul)"};
