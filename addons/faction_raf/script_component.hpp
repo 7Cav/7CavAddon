e7f25bf0d6316dd50eda4375693376ae3cc31ce4
ColdEvul <andreas.brostrom.ce@gmail.com>
2020-08-03 20:47:39 +0200

    Removed RAF

--- addons/faction_raf/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT faction_raf
-#define COMPONENT_BEAUTIFIED Faction Russian Armed Forces
-
-#include "\z\cav\addons\main\script_mod.hpp"
-#include "\z\cav\addons\main\script_macros.hpp"
