a954cdc53b2df20bbc3cece92c1ee2e4c8e6fce9
ColdEvul <andreas.brostrom.ce@gmail.com>
2020-08-03 20:37:09 +0200

    Added supplies

---
+++ addons/supplies/config.cpp
@@ -0,0 +1,21 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {
+        };
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "cav_main",
+            "cav_common"
+        };
+        author = ECSTRING(main,cavmodteam);
+        authors[] = {"Brostrom.A (Evul)"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.hpp"
