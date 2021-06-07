3b230c6488f849ebf1d6eeca98fda0659f8832d4
Andreas Brostrom <andreas.brostrom.ce@gmail.com>
2021-06-07 20:41:12 +0200

    added charlie weapons team

---
+++ addons/troops_charlie_weapons/CfgEditorSubcategories.hpp
@@ -0,0 +1,5 @@
+class CfgEditorSubcategories {
+    class Cav_EdSubcat_Personnel_Charlie_Weapons {
+        displayname = CSTRING(display); 
+    };
+};
---
+++ addons/troops_charlie_weapons/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT troops_charlie_weapons
+#define COMPONENT_BEAUTIFIED Charlie Co Weapons Team
+
+#include "\z\cav\addons\main\script_mod.hpp"
+#include "\z\cav\addons\main\script_macros.hpp"
