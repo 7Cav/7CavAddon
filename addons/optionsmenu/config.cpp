a97c63d2e3958337a10608bfb735ff04b2d88bb2
Andreas Brostrom <andreas.brostrom.ce@gmail.com>
2021-07-12 20:53:09 +0200

    Added optionsmenu

---
+++ addons/optionsmenu/config.cpp
@@ -0,0 +1,27 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "cav_main",
+            "cav_common"
+        };
+        author = ECSTRING(main,modteam);
+        authors[] = {"Brostrom.A (Evul)"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "gui\mainMenu.hpp"
+
+class CfgCommands {
+    allowedHTMLLoadURIs[] += {
+        "https://7cav.us/",
+        "https://7cav.us/enlist"
+    };
+};
