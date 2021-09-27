7150b058db0298c175800a2b222419b4508966be
Andreas Brostrom <andreas.brostrom.ce@gmail.com>
2021-09-27 17:34:19 +0200

    Added orbat

---
+++ addons/orbat/config.cpp
@@ -0,0 +1,33 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {
+            "Cav_Flag_00_Flag_F",
+            "Cav_Flag_00_Banner_F",
+            "Cav_Flag_01_Flag_F",
+            "Cav_Flag_01_Banner_F",
+            "Cav_Flag_02_Flag_F",
+            
+            "Cav_Flag_bravo_2_flag_F",
+            "Cav_Flag_bravo_2_banner_F",
+            "Cav_Flag_charlie_1_flag_F",
+            "Cav_Flag_charlie_1_banner_F",
+            "Cav_Flag_charlie_2_flag_F",
+            "Cav_Flag_charlie_2_banner_F"
+        };
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "cav_main",
+            "cav_common"
+        };
+        author = ECSTRING(main,modteam);
+        authors[] = {"Brostrom.A (Evul)"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgOrbat.hpp"
---
+++ addons/orbat/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT orbat
+#define COMPONENT_BEAUTIFIED Orbat
+
+#include "\z\cav\addons\main\script_mod.hpp"
+#include "\z\cav\addons\main\script_macros.hpp"
