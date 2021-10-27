ccebb232166b4299347a32fe8985a2f5103a0651
Andreas Brostrom <andreas.brostrom.ce@gmail.com>
2021-10-27 20:02:23 +0200

    fixed strings and hidde ndepricated groups

--- addons/troops_bravo_atlas/config.cpp
+++ addons/troops_bravo_atlas/config.cpp
@@ -4,20 +4,20 @@ class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
         units[] = {
-    "Cav_B_Bravo_Atlas_base_F",
-    "Cav_B_B_Atlas_Medic_TeamLeader_F",
-    "Cav_B_B_Atlas_Medic_CombatMedic_F",
-    "Cav_B_B_Atlas_Logistics_Officer_F",
-    "Cav_B_B_Atlas_Logistics_OpsOfficer_F",
-    "Cav_B_B_Atlas_Logistics_OpsNCO_F",
-    "Cav_B_B_Atlas_Logistics_TeamLeader_F",
-    "Cav_B_B_Atlas_Logistics_TeamMember_F",
-    "Cav_B_B_Atlas_Medic_TeamLeader_3_1_F",
-    "Cav_B_B_Atlas_Medic_TeamLeader_3_2_F",
-    "Cav_B_B_Atlas_Logistics_TeamLeader_3_3_F",
-    "Cav_B_B_Atlas_Logistics_TeamLeader_3_4_F",
-    "Cav_B_B_Atlas_Logistics_OpsNCO_3_5_F",
-    "Cav_B_B_Atlas_Logistics_OpsOfficer_3_6_F"
+            "Cav_B_Bravo_Atlas_base_F",
+            "Cav_B_B_Atlas_Medic_TeamLeader_F",
+            "Cav_B_B_Atlas_Medic_CombatMedic_F",
+            "Cav_B_B_Atlas_Logistics_Officer_F",
+            "Cav_B_B_Atlas_Logistics_OpsOfficer_F",
+            "Cav_B_B_Atlas_Logistics_OpsNCO_F",
+            "Cav_B_B_Atlas_Logistics_TeamLeader_F",
+            "Cav_B_B_Atlas_Logistics_TeamMember_F",
+            "Cav_B_B_Atlas_Medic_TeamLeader_3_1_F",
+            "Cav_B_B_Atlas_Medic_TeamLeader_3_2_F",
+            "Cav_B_B_Atlas_Logistics_TeamLeader_3_3_F",
+            "Cav_B_B_Atlas_Logistics_TeamLeader_3_4_F",
+            "Cav_B_B_Atlas_Logistics_OpsNCO_3_5_F",
+            "Cav_B_B_Atlas_Logistics_OpsOfficer_3_6_F"
         };
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
