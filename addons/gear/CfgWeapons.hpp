0813ad41b2b27b07875b6d77f34fce4af48a5f73
VinoEtCaseus <lmbstryker@gmail.com>
2022-05-13 13:32:44 -0400

    Memes

--- addons/gear/CfgWeapons.hpp
+++ addons/gear/CfgWeapons.hpp
@@ -117,4 +117,45 @@ class cfgWeapons {
             };
         };
     };
+	
+	class UniformItem;
+	
+	class Cav_B_RRD_Parade_Uniform : ItemCore
+	{
+		author = "Dunn.W";
+		scope = 2;
+		scopeCurator = 2;
+        scopeArsenal = 2;
+		displayName = "Cavalry Officer Parade Uniform (Decorated)";
+		picture = QPATHTOF(Data\UI\icon_h_RRDUniform.paa);
+		class ItemInfo : UniformItem
+		{
+			uniformClass = Cav_B_RRD_Parade;
+			containerClass = Supply40;
+			mass = 40;
+		};
+		
+	};
+	
+	class Cav_Stetson : ItemCore
+	{
+		author = "Richards.D";
+		scope = 2; /// scope needs to be 2 to have a visible class
+		scopeCurator = 2;
+        scopeArsenal = 2;
+		displayName  = "Cavarly Stetson"; /// how would the stuff be displayed in inventory and on ground
+		picture = QPATHTOF(Data\UI\icon_h_stetson_ca.paa); /// this looks fairly similar
+		model   = QPATHTOF(DAR_Stetson); /// what model does the cap use
+		hiddenSelections[] = {"camo"}; /// what selection in model could have different textures
+		hiddenSelectionsTextures[] = {QPATHTOF(Data\RRD_Uniform\Stetson_co.paa)}; /// what texture is going to be used
+		class ItemInfo: HeadgearItem
+		{
+			mass = 10; /// combined weight and volume of the cap, this equals to single magazine
+			uniformModel = QPATHTOF(DAR_Stetson);	/// what model is used for this cap
+			allowedSlots[] = {UNIFORM_SLOT, BACKPACK_SLOT, VEST_SLOT, HEADGEAR_SLOT};	/// this cap is so small and flexible that it fits everywhere
+			modelSides[] = {6}; /// available for all sides
+			hiddenSelections[] = {"camo"}; /// what selection in model could have different textures
+			hiddenSelectionsTextures[] = {QPATHTOF(Data\RRD_Uniform\Stetson_co.paa)};
+		};
+	};
 };
