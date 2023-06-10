6a45a2a0c923ff162fd2dfa04b294b1037055736
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-06-10 09:52:15 +0200

    Fixed hemett issues and stringtable over due

--- addons/flag/CfgVehicles.hpp
+++ addons/flag/CfgVehicles.hpp
@@ -8,18 +8,18 @@ class CfgVehicles {
     class Cav_Flag_00_Flag_F : FlagCarrier_Asym {
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(00_Display);
+        displayName = CSTRING(00_display);
         author = ECSTRING(main,modteam);
         //editorPreview = QPATHTOF(EditorPreviews\Cav_Flag_00_Flag_F.jpg);
 
         class EventHandlers {
-            init = (_this select 0) setFlagTexture QPATHTOF(data\flag_00_ca.paa);
+            init = QUOTE(_this select 0 setFlagTexture QPATHTOF(data\flag_00_ca.paa));
         };
     };
     class Cav_Flag_00_Banner_F : Banner_01_base_F {
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(00_Banner_Display);
+        displayName = CSTRING(00_banner_display);
         author = ECSTRING(main,modteam);
         //editorPreview = QPATHTOF(EditorPreviews\Cav_Flag_00_Banner_F.jpg);
 
@@ -30,18 +30,18 @@ class CfgVehicles {
     class Cav_Flag_01_Flag_F : FlagCarrier_Asym {
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(01_Display);
+        displayName = CSTRING(01_display);
         author = ECSTRING(main,modteam);
         //editorPreview = QPATHTOF(EditorPreviews\Cav_Flag_01_Flag_F.jpg);
 
         class EventHandlers {
-            init = (_this select 0) setFlagTexture QPATHTOF(data\flag_01_ca.paa);
+            init = QUOTE(_this select 0 setFlagTexture QPATHTOF(data\flag_01_ca.paa));
         };
     };
     class Cav_Flag_01_Banner_F : Banner_01_base_F {
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(01_Banner_Display);
+        displayName = CSTRING(01_banner_display);
         author = ECSTRING(main,modteam);
         //editorPreview = QPATHTOF(EditorPreviews\Cav_Flag_01_Banner_F.jpg);
 
@@ -53,40 +53,40 @@ class CfgVehicles {
     class Cav_Flag_02_Flag_F : FlagCarrier {
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(02_Display);
+        displayName = CSTRING(02_display);
         author = ECSTRING(main,modteam);
         //editorPreview = QPATHTOF(EditorPreviews\Cav_Flag_02_Flag_F.jpg);
 
         class EventHandlers {
-            init = (_this select 0) setFlagTexture QPATHTOF(data\flag_02_ca.paa);
+            init = QUOTE(_this select 0 setFlagTexture QPATHTOF(data\flag_02_ca.paa));
         };
     };
     class Cav_Flag_alpha_1_Flag_F : FlagCarrier {
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(alpha_1_flag);
+        displayName = CSTRING(alpha_1_display);
         author = ECSTRING(main,modteam);
         //editorPreview = QPATHTOF(EditorPreviews\Cav_Flag_02_Flag_F.jpg);
 
         class EventHandlers {
-            init = (_this select 0) setFlagTexture QPATHTOF(data\flag_alpha1_ca.paa);
+            init = QUOTE(_this select 0 setFlagTexture QPATHTOF(data\flag_alpha1_ca.paa));
         };
     };
     class Cav_Flag_bravo_2_flag_F : FlagCarrier_Asym {
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(bravo_2_flag);
+        displayName = CSTRING(bravo_2_display);
         author = ECSTRING(main,modteam);
         //editorPreview = QPATHTOF(EditorPreviews\Cav_Flag_01_Flag_F.jpg);
 
         class EventHandlers {
-            init = (_this select 0) setFlagTexture QPATHTOF(data\flag_bravo2_ca.paa);
+            init = QUOTE(_this select 0 setFlagTexture QPATHTOF(data\flag_bravo2_ca.paa));
         };
     };
     class Cav_Flag_bravo_2_banner_F : Banner_01_base_F {
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(bravo_2_banner);
+        displayName = CSTRING(bravo_2_banner_display);
         author = ECSTRING(main,modteam);
         //editorPreview = QPATHTOF(EditorPreviews\Cav_Flag_01_Banner_F.jpg);
 
@@ -97,18 +97,18 @@ class CfgVehicles {
     class Cav_Flag_charlie_1_flag_F : FlagCarrier_Asym {
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(charlie_1_flag);
+        displayName = CSTRING(charlie_1_display);
         author = ECSTRING(main,modteam);
         //editorPreview = QPATHTOF(EditorPreviews\Cav_Flag_01_Flag_F.jpg);
 
         class EventHandlers {
-            init = (_this select 0) setFlagTexture QPATHTOF(data\flag_charlie1_ca.paa);
+            init = QUOTE(_this select 0 setFlagTexture QPATHTOF(data\flag_charlie1_ca.paa));
         };
     };
     class Cav_Flag_charlie_1_banner_F : Banner_01_base_F {
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(charlie_1_banner);
+        displayName = CSTRING(charlie_1_banner_display);
         author = ECSTRING(main,modteam);
         //editorPreview = QPATHTOF(EditorPreviews\Cav_Flag_01_Banner_F.jpg);
 
@@ -119,18 +119,18 @@ class CfgVehicles {
     class Cav_Flag_charlie_2_flag_F : FlagCarrier_Asym {
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(charlie_2_flag);
+        displayName = CSTRING(charlie_2_display);
         author = ECSTRING(main,modteam);
         //editorPreview = QPATHTOF(EditorPreviews\Cav_Flag_01_Flag_F.jpg);
 
         class EventHandlers {
-            init = (_this select 0) setFlagTexture QPATHTOF(data\flag_charlie2_ca.paa);
+            init = QUOTE(_this select 0 setFlagTexture QPATHTOF(data\flag_charlie2_ca.paa));
         };
     };
     class Cav_Flag_charlie_2_banner_F : Banner_01_base_F {
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(charlie_2_banner);
+        displayName = CSTRING(charlie_2_banner_display);
         author = ECSTRING(main,modteam);
         //editorPreview = QPATHTOF(EditorPreviews\Cav_Flag_01_Banner_F.jpg);
 
