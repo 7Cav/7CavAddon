af1a7513a3eefafe90de918c42678266f38768b8
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-06-10 10:42:32 +0200

    Updated build

--- tools/deploy.ps1
+++ tools/deploy.ps1
@@ -1,4 +1,3 @@
-
 cd $PSScriptRoot
 cd ..
 
--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-#sed -i "s/#define VERSION.*/#define VERSION \"${VERSION_TAG}\"/" ../addons/main/script_version.hpp
+# Set version
+tagVersion=$(git describe --tags --abbrev=0)
+echo "Build version $tagVersion"
 
-hemtt build --release --ci
-hemtt build zip
+versionMajor = $tagVersion | cut -d. -f1
+versionMinor = $tagVersion | cut -d. -f2
+versionPatch = $tagVersion | cut -d. -f3
+versionBuild = 0
+
+
+
+sed -e "s/DevBuild/$tagVersion/g" "mod.cpp" > .hemtt/project.toml
+
+sed -e "s/major = 0/major = $versionMajor/g" ".hemtt/project.toml" > .hemtt/project.toml
+sed -e "s/minor = 0/minor = $versionMinor/g" ".hemtt/project.toml" > .hemtt/project.toml
+sed -e "s/patch = 0/patch = $versionPatch/g" ".hemtt/project.toml" > .hemtt/project.toml
+sed -e "s/build = 0/build = $versionBuild/g" ".hemtt/project.toml" > .hemtt/project.toml
+
+hemtt.exe release
+
+# Clean up
+#echo "Restoring version files..."
+#git checkout origin/master mod.cpp
+#git checkout origin/master .hemtt/project.toml
