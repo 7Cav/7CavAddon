b5109e59c1dd8f3e8548e5825c53ea04902eef0e
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-07-07 22:39:32 +0200

    Added dragoon

---
+++ addons/vehicles_dragoon/config.cpp
@@ -0,0 +1,51 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {
+            "cav_dragoon_F",
+            "cav_dragoon_Unarmed_F",
+            "cav_dragoon_Unarmed_WD",
+            "cav_dragoon_Unarmed_D",
+            "cav_dragoon_Unarmed_A",
+            "cav_dragoon_WD",
+            "cav_dragoon_WD_PA",
+            "cav_dragoon_WD_V1",
+            "cav_dragoon_WD_V2",
+            "cav_dragoon_WD_V3",
+            "cav_dragoon_WD_V4",
+            "cav_dragoon_WD_V5",
+            "cav_dragoon_WD_V6",
+            "cav_dragoon_WD_WH7",
+            "cav_dragoon_D",
+            "cav_dragoon_D_V1",
+            "cav_dragoon_D_V2",
+            "cav_dragoon_D_V3",
+            "cav_dragoon_D_V4",
+            "cav_dragoon_D_V5",
+            "cav_dragoon_D_V6",
+            "cav_dragoon_D_WH7",
+            "cav_dragoon_A",
+            "cav_dragoon_A_V1",
+            "cav_dragoon_A_V2",
+            "cav_dragoon_A_V3",
+            "cav_dragoon_A_V4",
+            "cav_dragoon_A_V5",
+            "cav_dragoon_A_V6",
+            "cav_dragoon_A_WH7"
+        };
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "cav_main",
+            "cav_common"
+        };
+        author = ECSTRING(main,modteam);
+        authors[] = {"Narramoor.A"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.hpp"
