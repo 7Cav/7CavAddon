4c8c66b5dbcd0845b21487b0f00961538a51f650
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-07-07 23:50:02 +0200

    Added category and fixed issues

---
+++ addons/vehicles_dragoon/CfgEditorSubcategories.hpp
@@ -0,0 +1,11 @@
+class CfgEditorSubcategories {
+    class Cav_EdSubcat_vehicles_woodland {
+        displayname = CSTRING(Cav_EdSubcat_vehicles_woodland); 
+    };
+    class Cav_EdSubcat_vehicles_desert {
+        displayname = CSTRING(Cav_EdSubcat_vehicles_desert); 
+    };
+    class Cav_EdSubcat_vehicles_arctic {
+        displayname = CSTRING(Cav_EdSubcat_vehicles_arctic); 
+    };
+};
