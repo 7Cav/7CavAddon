62c38efdf64383172c68cfadce6dcc0c51d3b0a2
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-08-25 14:40:12 +0200

    Added empty charlie module

---
+++ addons/infantry_charlie/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT infantry_charlie
+#define COMPONENT_BEAUTIFIED Infantry Charlie Company
+
+#include "\z\cav\addons\main\script_mod.hpp"
+#include "\z\cav\addons\main\script_macros.hpp"
