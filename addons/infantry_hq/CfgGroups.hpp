7b6a1469246c4379628c2103b410b0ee7df5a0cc
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-01-22 11:28:39 +0100

    Added HQ

---
+++ addons/infantry_hq/CfgGroups.hpp
@@ -0,0 +1,7 @@
+class CfgGroups {
+    class West {
+        class cav_blu_usa_f {
+            name = ECSTRING(common,faction_display);
+        };
+    };
+};
---
+++ addons/infantry_hq/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT infantry_hq
+#define COMPONENT_BEAUTIFIED Infantry HQ
+
+#include "\z\cav\addons\main\script_mod.hpp"
+#include "\z\cav\addons\main\script_macros.hpp"
