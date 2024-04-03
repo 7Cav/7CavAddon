3fce3137630aa0f2ddf202f9b68d94c71ed0b23d
Jonah Pool <jpool.school@gmail.com>
2024-04-03 17:03:06 -0400

    Further weapons updates as well as convention update

--- addons/vehicles_dragoon/CfgMagazines.hpp
+++ addons/vehicles_dragoon/CfgMagazines.hpp
@@ -1,37 +1,38 @@
 class CfgMagazines {
     class 140Rnd_30mm_MP_shells_Tracer_Red;
-    class 78Rnd_30mm_MP_shells_Tracer_Red: 140Rnd_30mm_MP_shells_Tracer_Red {
-        displayName = CSTRING(78Rnd_30mm_MP_shells_Tracer_Red);
-        displayNameShort = CSTRING(78Rnd_30mm_MP_shells_Tracer_Red_dispShort);
+    class cav_78Rnd_30mm_MP_shells_Tracer_Red: 140Rnd_30mm_MP_shells_Tracer_Red {
+        displayName = CSTRING(cav_78Rnd_30mm_MP_shells_Tracer_Red);
+        displayNameShort = CSTRING(cav_78Rnd_30mm_MP_shells_Tracer_Red_dispShort);
         displayNameMFDFormat = "MP-T";
         count=78;
     };
     class 60Rnd_30mm_APFSDS_shells_Tracer_Red;
-    class 78Rnd_30mm_APFSDS_shells_Tracer_Red: 60Rnd_30mm_APFSDS_shells_Tracer_Red {
-        displayName = CSTRING(78Rnd_30mm_APFSDS_shells_Tracer_Red);
-        displayNameShort = CSTRING(78Rnd_30mm_APFSDS_shells_Tracer_Red_dispShort);
+    class cav_78Rnd_30mm_APFSDS_shells_Tracer_Red: 60Rnd_30mm_APFSDS_shells_Tracer_Red {
+        displayName = CSTRING(cav_78Rnd_30mm_APFSDS_shells_Tracer_Red);
+        displayNameShort = CSTRING(cav_78Rnd_30mm_APFSDS_shells_Tracer_Red_dispShort);
         displayNameMFDFormat = "APFSDS-T";
         count=78;
     };
-    class 78Rnd_30mm_HEI_shells_Tracer_Red: 78Rnd_30mm_MP_shells_Tracer_Red {
+    class cav_78Rnd_30mm_HEI_shells_Tracer_Red: cav_78Rnd_30mm_MP_shells_Tracer_Red {
         ammo = "cav_B_30mm_HEI_Tracer_Red";
         displayNameMFDFormat = "HEI-T";
-        displayName = CSTRING(78Rnd_30mm_HEI_shells_Tracer_Red);
-        displayNameShort = CSTRING(78Rnd_30mm_HEI_shells_Tracer_Red_dispShort);
+        displayName = CSTRING(cav_78Rnd_30mm_HEI_shells_Tracer_Red);
+        displayNameShort = CSTRING(cav_78Rnd_30mm_HEI_shells_Tracer_Red_dispShort);
         initSpeed=1100;
     };
     class 4Rnd_Titan_long_missiles;
-    class cav_2Rnd_GAA_missiles: 4Rnd_Titan_long_missiles {
-        displayName = CSTRING(cav_2Rnd_GAA_missiles);
-        displayNameShort = CSTRING(cav_2Rnd_GAA_missiles_dispShort);
+    class cav_2Rnd_fim92_missiles: 4Rnd_Titan_long_missiles {
+        displayName = CSTRING(cav_2Rnd_fim92_missiles);
+        displayNameShort = CSTRING(cav_2Rnd_fim92_missiles_dispShort);
+        ammo = "rhs_ammo_fim92_missile";
         count=2;
     };
     // TODO: ACE FCS Air Burst Mags
-    class 78Rnd_30mm_HEABT_shells_Tracer_Red: 78Rnd_30mm_MP_shells_Tracer_Red {
+    class cav_78Rnd_30mm_HEABT_shells_Tracer_Red: cav_78Rnd_30mm_MP_shells_Tracer_Red {
         ammo="cav_B_30mm_HEAB_Tracer_Red";
         displayNameMFDFormat="HEAB-T";
-        displayName=CSTRING(78Rnd_30mm_HEAB_shells_Tracer_Red);
-        displayNameShort=CSTRING(78Rnd_30mm_HEAB_shells_Tracer_Red_dispShort);
+        displayName=CSTRING(cav_78Rnd_30mm_HEAB_shells_Tracer_Red);
+        displayNameShort=CSTRING(cav_78Rnd_30mm_HEAB_shells_Tracer_Red_dispShort);
     };
     class 200Rnd_762x51_Belt_Red;
     class cav_mag_300Rnd_762x51_Belt_Red: 200Rnd_762x51_Belt_Red {
--- addons/vehicles_dragoon/CfgWeapons.hpp
+++ addons/vehicles_dragoon/CfgWeapons.hpp
@@ -12,15 +12,21 @@ class CfgWeapons {
     {
         class HE: HE
         {
-            magazines[] += {"78Rnd_30mm_MP_shells_Tracer_Red","78Rnd_30mm_HEI_shells_Tracer_Red"};
+            magazines[] += {"cav_78Rnd_30mm_MP_shells_Tracer_Red","cav_78Rnd_30mm_HEI_shells_Tracer_Red"};
         };
         class AP: AP
         {
-            magazines[] += {"78Rnd_30mm_APFSDS_shells_Tracer_Red"};
+            magazines[] += {"cav_78Rnd_30mm_APFSDS_shells_Tracer_Red"};
         };
     };
     class missiles_titan;
     class cav_dragoon_missiles_AA: missiles_titan {
-        magazines[] = {"4Rnd_Titan_long_missiles"};
+        displayName = CSTRING(cav_dragoon_missiles_AA);
+        magazines[] = {"cav_2Rnd_fim92_missiles","rhs_fim92_mag"};
+    };
+    class LMG_coax_ext;
+    class cav_dragoon_coax_m240: LMG_coax_ext {
+        displayName = CSTRING(cav_dragoon_coax_m240);
+        magazines[] += {"cav_mag_300Rnd_762x51_Belt_Red"};
     };
 };
