3f764ca9de48761ac20e7a25cbe997da9a045c23
Broström.A | Evul <andreas.brostrom.ce@gmail.com>
2024-04-28 21:47:12 +0000

    Added predefined cav respawn templates (#234)
    * Added predefined cav respawn templates
    
    * Fixed string

---
+++ addons/respawn/CfgRespawnTemplates.hpp
@@ -0,0 +1,18 @@
+class CfgRespawnTemplates {
+    class cav_respawn {
+        displayName = CSTRING(cav_respawn);
+        onPlayerKilled = "BIS_fnc_respawnMenuPosition";
+        onPlayerRespawn = "BIS_fnc_respawnMenuPosition";
+        respawn = "BASE";
+        respawnDelay = 4;
+        respawnTypes[] = {2,3};
+        respawnOnStart = -1;
+    };
+    class cav_onelife {
+        displayName = CSTRING(cav_onelife);
+        onPlayerKilled = "BIS_fnc_respawnSpectator";
+        onPlayerRespawn = "BIS_fnc_respawnSpectator";
+        respawnOnStart = 30000;
+        respawnTypes[] = {1,2,3};
+    };
+};
