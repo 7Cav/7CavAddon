4d7b3ffc85fe714b1fc62e9b995087bb6fd23d8e
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-07-21 21:45:35 +0200

    Added mission controller

--- addons/infantry_hq/CfgVehicles.hpp
+++ addons/infantry_hq/CfgVehicles.hpp
@@ -1,2 +1,21 @@
 class CfgVehicles {
+    class Cav_B_MissionController_F;
+    class Cav_B_MissionController_1_F: Cav_B_MissionController_F {
+        _generalMacro = "Cav_B_MissionController_1_F";
+        scope = 2;
+        scopeCurator = 2;
+        displayName = CSTRING(Cav_B_MissionController_1_F);
+
+        cav_company = "zeus";
+    };
+    class Cav_B_MissionController_2_F: Cav_B_MissionController_F {
+        _generalMacro = "Cav_B_MissionController_2_F";
+        scope = 2;
+        scopeCurator = 2;
+        displayName = CSTRING(Cav_B_MissionController_2_F);
+
+        editorSubcategory = "Cav_EdSubcat_Infantry_HQ";
+
+        cav_company = "zeus";
+    };
 };
