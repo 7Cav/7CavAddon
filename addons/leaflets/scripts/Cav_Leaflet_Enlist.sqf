2bd910a19616b5c6866ada63f34cd9f65ff7851d
Broström.A | Evul <andreas.brostrom.ce@gmail.com>
2024-07-21 23:15:38 +0200

    Added leaflets and poster for public relations (#250)
    * Added leaflets system
    
    * Added enlist posters and leaflet item
    
    * Fixed BIS tab
    
    * Fixed copy paste tab

---
+++ addons/leaflets/scripts/Cav_Leaflet_Enlist.sqf
@@ -0,0 +1,3 @@
+if ((_this select 2) < 1) then {
+    ["init",[_this,"Cav_Enlist"]] remoteExec ["bis_fnc_initLeaflet"];
+};
---
+++ addons/objects/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT objects
+#define COMPONENT_BEAUTIFIED Objects
+
+#include "\z\cav\addons\main\script_mod.hpp"
+#include "\z\cav\addons\main\script_macros.hpp"
